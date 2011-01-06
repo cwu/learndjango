33866f4c4fb155408902358cfeb6c8566803f304
Chris Wu <chris.wu225@gmail.com>
2011-01-06 00:49:40 -0500

    removed unessesary foo.rb

--- media/css/foo.rb
+++
@@ -1,9 +0,0 @@
-
-(0..19).each do |row| 
-  (0..24).each do |col| 
-    puts ".p_img_#{row}_#{col} { " +
-         "background: url('/static/img/pokemon_sprites.png') no-repeat " +
-         "scroll #{-col*80}px #{-row*80}px transparent; }"
-       
-  end
-end
